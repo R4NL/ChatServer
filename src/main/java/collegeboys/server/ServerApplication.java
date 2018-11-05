0a1be3c58504f712613d577748b6dd013b271737
aleshka <Alexfenin1234@gmail.com>
2018-11-06 00:35:46 +0200

    Just add some test

--- src/main/java/collegeboys/server/ServerApplication.java
+++ src/main/java/collegeboys/server/ServerApplication.java
@@ -1,12 +1,18 @@
 package collegeboys.server;
 
+
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.info.ProjectInfoProperties;
 
 
 @SpringBootApplication
 public class ServerApplication {
+
     public static void main(String[] args) {
+
+
         SpringApplication.run(ServerApplication.class, args);
     }
 }
---
+++ src/test/java/collegeboys/server/entityTest/MessageTest.java
@@ -0,0 +1,34 @@
+package collegeboys.server.entityTest;
+
+import collegeboys.server.entity.Message;
+import collegeboys.server.entity.Person;
+import org.junit.Test;
+
+import java.util.Date;
+
+import static org.junit.Assert.assertTrue;
+
+public class MessageTest {
+    @Test
+    public void messageTestOne() {
+        Message message = new Message();
+        assertTrue(message.getText() == null);
+        assertTrue(message.getDate() == null);
+        assertTrue(message.getPerson() == null);
+    }
+
+    @Test
+    public void messageTestTwo() {
+        Message message = new Message("Hello world", new Date(),
+                new Person("TheAleshka", "1234"));
+        assertTrue(message.getText() != null);
+        assertTrue(message.getDate() != null);
+        assertTrue(message.getPerson() != null);
+    }
+    @Test
+    public void messageTestThree() {
+        Message message = new Message("Hello world", new Date(),
+                new Person("TheAleshka", "1234"));
+        assertTrue(message.getText() == "Hello world");
+    }
+}
