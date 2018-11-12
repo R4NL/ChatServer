f6de8e884e84364f94dedd818b5d04cdd878b089
TheAlEShKA <alexfenin1234@gmail.com>
2018-11-12 22:57:00 +0200

    Merge branch 'master' of C:\Users\Леша\IdeaProjects\ChatServer with conflicts.

--- src/test/java/collegeboys/server/ServerTest/ServerTest.java
+++ src/test/java/collegeboys/server/ServerTest/ServerTest.java
@@ -7,12 +7,14 @@ import collegeboys.server.Server.MessageHandler.MessageRender;
 import collegeboys.server.entity.Message;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import static junit.framework.TestCase.assertFalse;
 import static junit.framework.TestCase.assertTrue;
 import static org.mockito.Mockito.mock;
 
+import collegeboys.server.entity.Person;
 import org.junit.Test;
 
 public class ServerTest {
@@ -31,22 +33,6 @@ public class ServerTest {
         ChatServer chatServer = new ChatServer();
         assertTrue(chatServer.init());
     }
-    
-    @Test
-    public void overrideTest() {
-        MessageProvider provider = mock(MessageProvider.class);
-        List<SingleConnection> connections = new ArrayList<>();
-        List<Message> messages = new ArrayList<>();
-        SingleConnection singleConnection = new SingleConnection(provider, connections, messages, 885553535);
-        assertFalse(singleConnection.equals(null));
-        assertFalse(singleConnection.hashCode() > 0);
-    }
-    
-     @Test
-    public void messageProviderTest(){
-        Message message=new Message("Test",new Date(),new Person("Solarich","Solarich"));
-        MessageRender render=new MessageRender();
-        assertTrue(render.renderMessage(message).contains("Test"));
-        assertTrue(render.renderMessage(message).contains("Solarich"));
-    }
+
+
 }
