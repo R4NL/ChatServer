0758e94d4b10b7ee66ab812213be6f0cc0ff27d1
just4faan <43173584+just4faan@users.noreply.github.com>
2018-11-12 22:00:35 +0200

    add test and test with mock

--- src/test/java/collegeboys/server/ServerTest/ServerTest.java
+++ src/test/java/collegeboys/server/ServerTest/ServerTest.java
@@ -1,4 +1,28 @@
 package collegeboys.server.ServerTest;
+import collegeboys.server.Server.ChatServer;
+import collegeboys.server.entity.Message;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static junit.framework.TestCase.assertTrue;
+import static org.mockito.Mockito.mock;
+import org.junit.Test;
 
 public class ServerTest {
+    @Test
+    public void constructorTest(){
+        MessageProvider provider=mock(MessageProvider.class);
+        List<SingleConnection> connections=new ArrayList<>();
+        List<Message> messages=new ArrayList<>();
+        SingleConnection singleConnection=new SingleConnection(provider,connections,messages,885553535);
+        assertTrue(singleConnection.getMessageProvider()!=null);
+        assertTrue(singleConnection.toString()!=null);
+    }
+}
+  @Test
+    public void chatServerTest(){
+        ChatServer chatServer=new ChatServer();
+       assertTrue(chatServer.init());
+    }
 }
